3ef9b17bd1f2e39b9481e3fe1dc5d6bb68a13ca0
ShivanandMutnale <shivanand.mutnale@capgemini.com>
2022-10-07 13:02:48 +0530

    Test

---
+++ src/com/test/Git.groovy
@@ -0,0 +1,22 @@
+package com.test
+
+class Git implements Serializable {
+
+    private final def script
+
+    Git(def script) {
+        this.script = script
+    }
+
+    def checkout(String repo) {
+        this.script.git credentialsId: Constants.JENKINS_GITHUB_CREDENTIALS_ID, url: "${repo}"
+    }
+
+    String commitHash() {
+        return this.script.sh(script: getLatestGitCommitHashCommand(), returnStdout: true).trim()
+    }
+
+    private static String getLatestGitCommitHashCommand() {
+        return "git rev-parse HEAD"
+    }
+}
--- vars/buildSubscriberPipeline.groovy
+++ vars/buildSubscriberPipeline.groovy
@@ -1,8 +1,11 @@
 def call() {
   node {
-      def p = pipelineCfg()
-      stage('Checkout') {
-        println "TEST "
-      }
+       stage("Checkout") {
+            new Git(this).checkout("${args.repo}")
+        }
+        def p = pipelineCfg()
+        stage('Process Properties') {
+            println "TEST "
+        }
   }
 }
