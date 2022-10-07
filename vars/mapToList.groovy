24128aaeeae13139a9876e33cc4ebec4c192cfc2
ShivanandMutnale <shivanand.mutnale@capgemini.com>
2022-10-07 20:30:45 +0530

    test

--- vars/mapToList.groovy
+++ vars/mapToList.groovy
@@ -1,6 +1,7 @@
 def call(depmap) {
     def dlist = []
     for (def entry2 in depmap) {
+        println entry2 
         dlist.add(new java.util.AbstractMap.SimpleImmutableEntry(entry2.key.toString(), entry2.value.toString()))
     }
     dlist
