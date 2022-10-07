42e25644002c22092a21d16fa3d4d356f9d2adc0
ShivanandMutnale <shivanand.mutnale@capgemini.com>
2022-10-07 17:13:43 +0530

    test

--- vars/buildSubscriberPipeline.groovy
+++ vars/buildSubscriberPipeline.groovy
@@ -3,6 +3,7 @@ import com.test.Git
 def call(Map args=[:]) {
   node {
        stage("Checkout") {
+            println "REPO : ${args.repo}"
             new Git(this).checkout("${args.repo}")
         }
         def p = pipelineCfg()
