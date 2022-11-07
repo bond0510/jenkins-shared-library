73a1bbed900666ad837d5568bf227b20356b4d82
ShivanandMutnale <shivanand.mutnale@capgemini.com>
2022-11-07 22:51:29 +0530

    Modified buildCommonK8SPipeline.groovy

--- vars/buildCommonK8SPipeline.groovy
+++ vars/buildCommonK8SPipeline.groovy
@@ -5,7 +5,7 @@ def call(Map args=[:]) {
   node {
 		stage('Code Checkout') { // for display purposes
 			println "REPO : ${args.repo}"
-			checkout poll: true, scm: [$class: 'GitSCM', branches: [[name: 'refs/heads/test']], doGenerateSubmoduleConfigurations: true, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'Bitbucket_SSHkey', url: '${args.repo}']]]
+			checkout poll: true, scm: [$class: 'GitSCM', branches: [[name: 'refs/heads/test']], doGenerateSubmoduleConfigurations: true, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'Bitbucket_SSHkey', url: "${args.repo}"]]]
 		}
         def p = pipelineConfig()
         stage('Process Properties') {
