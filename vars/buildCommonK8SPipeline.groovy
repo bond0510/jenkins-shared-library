4d468b7b29674fadd838ba95dce0d74b540c144d
ShivanandMutnale <shivanand.mutnale@capgemini.com>
2022-11-08 17:10:44 +0530

    modified buildCommonK8SPipeline.groovy

--- vars/buildCommonK8SPipeline.groovy
+++ vars/buildCommonK8SPipeline.groovy
@@ -26,7 +26,8 @@ def call(Map args=[:]) {
 						def placeHolder="${command}_VAL"
 						withCredentials([string(credentialsId: key, variable: 'VALUE')]) {
 						    dir('common'){
-                                sh 'sed -i "s/"\"${placeHolder}\""/$VALUE/g" "\"${fileName}\""'
+                                strCommand="sed -i "s/${placeHolder}/$VALUE/g" ${fileName}"
+                                sh '${strCommand}'
 						    }
                         }
 				}
