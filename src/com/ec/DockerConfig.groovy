b1468ee0864fbe0a64304eab4ce3171eea85da5c
ShivanandMutnale <shivanand.mutnale@capgemini.com>
2022-11-28 18:29:50 +0530

    Modified for docker build

--- src/com/ec/DockerConfig.groovy
+++ src/com/ec/DockerConfig.groovy
@@ -4,9 +4,10 @@ class DockerConfig {
 
     def yaml
     DockerConfig ( def yaml){
-        this.yaml=yaml
+        this.yaml = yaml
     }
     def imageName() {
+        echo "${yaml.dockerConfig.dockerImageName}"
         "${yaml.dockerConfig.dockerImageName}".toLowerCase()
     }
 }
