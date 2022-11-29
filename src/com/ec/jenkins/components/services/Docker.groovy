95a06568df0a304c4f41147ea1d3aeb21e095e05
ShivanandMutnale <shivanand.mutnale@capgemini.com>
2022-11-29 17:23:58 +0530

    Modified for docker build

--- src/com/ec/jenkins/components/services/Docker.groovy
+++ src/com/ec/jenkins/components/services/Docker.groovy
@@ -9,9 +9,9 @@ class Docker {
 
     Docker(steps) {
         this.steps = steps
-        
+        steps.env.BUILD_NUMBER = steps.sh(script: '${BUILD_NUMBER}', returnStdout: true).trim()
         echo "build URL is ${steps.env.BUILD_NUMBER}"
-        echo "workspace directory is ${steps.env.WORKSPACE}/cardxPSD2/pipline.yaml"
+        
     }
 
 }
