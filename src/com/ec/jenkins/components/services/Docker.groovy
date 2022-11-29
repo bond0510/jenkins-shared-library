c026619eff2baf11e219b859f7423c6e5d45489d
ShivanandMutnale <shivanand.mutnale@capgemini.com>
2022-11-29 19:48:53 +0530

    Modified for docker build

--- src/com/ec/jenkins/components/services/Docker.groovy
+++ src/com/ec/jenkins/components/services/Docker.groovy
@@ -5,9 +5,10 @@ import com.ec.jenkins.components.parser.ConfigParser
 
 class Docker {
 
-    def build ( String imageName, List<String> tags=['latest'] ) {
-
+    def build ( def imageName, List<String> tags=['latest'] ) {
+        
         tags.each { tag ->
+            echo "DOCKER IMAGE NAME : ${imageName}:${tag}"
             sh "docker build --file=docker/Dockerfile.remote -t ${imageName}:${tag} ."
         }
     }
