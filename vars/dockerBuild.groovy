a69dae4bd82419a9f2d1e01245f354f82e6ee9ed
ShivanandMutnale <shivanand.mutnale@capgemini.com>
2022-11-29 20:00:29 +0530

    Modified for docker build

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -26,6 +26,6 @@ def call ( String env , String workDir) {
 def buildDockerImage( String imageName, String TAG_VERSION ){
     def docker = new Docker()
     echo "DOCKER IMAGE NAME : ${imageName}:${TAG_VERSION}"
-    List<String> tags = [TAG_VERSION ,'latest']
+    List<String> tags = ["${TAG_VERSION}" ,'latest']
     docker.build(imageName,tags)
 }
