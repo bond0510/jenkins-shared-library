21123e4d526473749c95059f598467f626031317
ShivanandMutnale <shivanand.mutnale@capgemini.com>
2022-11-29 17:34:36 +0530

    Modified for build docker

--- src/com/ec/jenkins/components/services/Docker.groovy
+++ src/com/ec/jenkins/components/services/Docker.groovy
@@ -9,7 +9,7 @@ class Docker {
 
     Docker( steps , Closure body=null ) {
         this.steps = steps
-        steps.env.BUILD_NUMBER = steps.sh(script: '${BUILD_NUMBER}', returnStdout: true).trim()
+        echo "build URL is ${env.BUILD_NUMBER}"
         echo "build URL is ${steps.env.BUILD_NUMBER}"
         
     }
