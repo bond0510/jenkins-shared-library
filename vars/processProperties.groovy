688ba433e1dbb02957c21cdc537fe1173d7eae10
ShivanandMutnale <shivanand.mutnale@capgemini.com>
2022-12-01 13:39:54 +0530

    Modfied for process properties

--- vars/processProperties.groovy
+++ vars/processProperties.groovy
@@ -3,8 +3,6 @@ import com.ec.jenkins.components.Utils
 def call(Closure body=null) {
 
     def config = new Utils().parseConfig(body) 
-    def jdkName = config?.jdkName ?: "jdk"
-    def namespace=config?.params.NAMESPACES
-    println jdkName namespace
+    println config 
 
 }
