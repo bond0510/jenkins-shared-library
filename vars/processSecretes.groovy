012ec128c9491080f2c221e5633009279b34aa95
ShivanandMutnale <shivanand.mutnale@capgemini.com>
2022-12-08 10:57:50 +0530

    Modified to delete docker image before creating new one

---
+++ vars/processSecretes.groovy
@@ -0,0 +1,27 @@
+import com.ec.jenkins.components.ProjectConfiguration
+import com.ec.jenkins.components.Property
+
+void call( Map args=[:] ) {
+    ProjectConfiguration projectConfig = args?.projectConfig
+    List<Property> propertyList = projectConfig?.secretes?.props
+    if ( propertyList != null ) {
+        propertyList.each { prop ->
+            println prop.name
+            prop.keys.each { key ->
+                script {
+                    env.fileName = prop.name
+                    if ( args?.containsKey (key) ) {
+                        env.propertyKey = key
+                        value = args?.get (key)
+                        println "${key} value is ${value}"
+                    } else {
+                        println "No property defined for ${key}"
+                    }
+                }
+                sh ' echo  $fileName $propertyKey '
+            }
+        }
+    } else {
+        println 'No properties are configured for this pipeline'
+    }
+}
