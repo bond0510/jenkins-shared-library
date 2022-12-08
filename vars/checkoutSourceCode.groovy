a0d13116d300a044ba456181525be7977c06076d
ShivanandMutnale <shivanand.mutnale@capgemini.com>
2022-12-08 15:05:08 +0530

    Modified for code check out

--- vars/checkoutSourceCode.groovy
+++ vars/checkoutSourceCode.groovy
@@ -1,7 +1,8 @@
 import com.ec.jenkins.components.services.Git
 
 def call ( Map args=[:]) {
-    println args.repo args.env
+    println args.repo 
+    println args.env
     new Git().checkout(args)
 
 }
