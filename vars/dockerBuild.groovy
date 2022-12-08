501f575d2c3fa5bd99aa3230cf501ccf8fe5bcb2
ShivanandMutnale <shivanand.mutnale@capgemini.com>
2022-12-08 12:56:18 +0530

    Modified to delete docker image

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -22,10 +22,8 @@ void call ( Map args=[:] ) {
 void buildDockerImage ( String imageName, List<String> tags ) { 
     tags.each { tag ->
         sh """
-            docker image inspect ${imageName}:${tag} >/dev/null 2>&1 && imagePresent=true|| imagePresent=false
-            if [[$imagePresent]]; then
-               docker images  ${imageName}:${tag}  | xargs --no-run-if-empty docker rmi -f 
-            fi
+            docker image inspect ${imageName}:${tag} >/dev/null 2>&1 && docker images  ${imageName}:${tag}  | xargs --no-run-if-empty docker rmi -f || echo NO
+
         """
     }
     tags.each { tag ->
