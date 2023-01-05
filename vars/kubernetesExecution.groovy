053062b683d577992f27ddccea5fd4a8eee06829
ShivanandMutnale <shivanand.mutnale@capgemini.com>
2023-01-05 17:34:16 +0530

    Modified for k8s steps

--- vars/kubernetesExecution.groovy
+++ vars/kubernetesExecution.groovy
@@ -7,31 +7,47 @@ void call( Map args=[:] ) {
     String dockerImageName = dockerCfg.imageName
     String dockerImageTag = dockerCfg.tag
     String envName = args.env
-    String path =args.workingDirectory
+    String path = args.workingDirectory
     updateAppYaml (dockerImageName , dockerImageTag , envName,path)
 }
 
 void updateAppYaml(String dockerImageName , String dockerImageTag ,String envName ,String path) {
-        file = "${path}/k8s/app.yaml"
-        if ( fileExists (file)) {
-           env.fileName = file
-           env.IMAGENAME = dockerImageName
-           env.IMAGETAG = dockerImageTag
-           env.ENVNAME = getEnv(envName)
-           sh 'sed -i "s/ENV/$ENVNAME/g" $fileName'
-           sh 'sed -i "s/DOCKER_IMAGE_NAME/$IMAGENAME/g" $fileName'
-           sh 'sed -i "s/TAG/$IMAGETAG/g" $fileName'
-        }
+    file = "${path}/k8s/app.yaml"
+    if ( fileExists (file)) {
+        env.fileName = file
+        env.IMAGENAME = dockerImageName
+        env.IMAGETAG = dockerImageTag
+        env.ENVNAME = getEnv(envName)
+        sh 'sed -i "s/ENV/$ENVNAME/g" $fileName'
+        sh 'sed -i "s/DOCKER_IMAGE_NAME/$IMAGENAME/g" $fileName'
+        sh 'sed -i "s/TAG/$IMAGETAG/g" $fileName'
+    }
 }
-String getEnv(String name){
+
+String getEnv(String name) {
    switch (name) {
         case ['dev' ,'test']:
-           return "tstenv"
+           return 'tstenv'
         case 'stage':
-           return "stgenv"
+           return 'stgenv'
         case 'prod' :
-           return "proenv"
+           return 'proenv'
         default:
-            return "tstenv"
+            return 'tstenv'
+    }
+}
+
+String getK8SContext( String env ) {
+    switch (env) {
+        case 'dev' :
+           return 'oradev'
+        case 'test' :
+           return 'oratest'
+        case 'stage':
+           return 'orastage'
+        case 'prod' :
+           return 'oraprod'
+        default:
+            return 'oradev'
     }
 }
